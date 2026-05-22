7b29e17ede9e647b46e16e3b4d23768cfa96d1e8
em411 <7682404+em411@users.noreply.github.com>
2026-05-22 12:49:02 +0200

    refactor: downgrade Bundle and Extension to Symfony 4.4

--- src/DependencyInjection/FeatureFlagExtension.php
+++ src/DependencyInjection/FeatureFlagExtension.php
@@ -2,62 +2,26 @@
 
 namespace Ajgarlag\FeatureFlagBundle\DependencyInjection;
 
-use Ajgarlag\FeatureFlagBundle\Attribute\AsFeature;
 use Ajgarlag\FeatureFlagBundle\Provider\ProviderInterface;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Exception\LogicException;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
-use Symfony\Component\Routing\Router;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Twig\Environment;
 
 class FeatureFlagExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new PhpFileLoader($container, new FileLocator(\dirname(__DIR__).'/../config'));
+        $loader = new XmlFileLoader($container, new FileLocator(\dirname(__DIR__).'/../config'));
 
-        $loader->load('feature_flag.php');
+        $loader->load('feature_flag.xml');
 
         $container->registerForAutoconfiguration(ProviderInterface::class)
-            ->addTag('ajgarlag.feature_flag.provider')
-        ;
+            ->addTag('ajgarlag.feature_flag.provider');
 
-        $container->registerAttributeForAutoconfiguration(
-            AsFeature::class,
-            static function (ChildDefinition $definition, AsFeature $attribute, \ReflectionClass|\ReflectionMethod $reflector): void {
-                $featureName = $attribute->name;
-
-                if ($reflector instanceof \ReflectionClass) {
-                    $className = $reflector->getName();
-                    $method = $attribute->method ?? '__invoke';
-
-                    $featureName ??= $className;
-                } else {
-                    $className = $reflector->getDeclaringClass()->getName();
-                    if (null !== $attribute->method && $reflector->getName() !== $attribute->method) {
-                        throw new LogicException(\sprintf('Using the #[%s(method: "%s")] attribute on a method is not valid. Either remove the method value or move this to the top of the class (%s).', AsFeature::class, $attribute->method, $className));
-                    }
-
-                    $method = $reflector->getName();
-                    $featureName ??= "{$className}::{$method}";
-                }
-
-                $definition->addTag('ajgarlag.feature_flag.feature', [
-                    'feature' => $featureName,
-                    'method' => $method,
-                ]);
-            },
-        );
-
-        if (ContainerBuilder::willBeAvailable('symfony/routing', Router::class, ['symfony/framework-bundle', 'symfony/routing'])) {
-            $loader->load('feature_flag_routing.php');
-        }
-
-        if (ContainerBuilder::willBeAvailable('symfony/twig', Environment::class, ['symfony/framework-bundle', 'symfony/twig'])) {
-            $loader->load('feature_flag_twig.php');
+        if (class_exists(Environment::class)) {
+            $loader->load('feature_flag_twig.xml');
         }
     }
 }
