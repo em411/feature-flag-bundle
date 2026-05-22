2e0c3793b235b35b1644719a12f8ef1a9369a412
em411 <7682404+em411@users.noreply.github.com>
2026-05-22 12:51:58 +0200

    refactor: scan @AsFeature annotations and wire a lazy service locator

--- src/DependencyInjection/Compiler/FeatureFlagPass.php
+++ src/DependencyInjection/Compiler/FeatureFlagPass.php
@@ -2,13 +2,18 @@
 
 namespace Ajgarlag\FeatureFlagBundle\DependencyInjection\Compiler;
 
+use Ajgarlag\FeatureFlagBundle\Attribute\AsFeature;
+use Doctrine\Common\Annotations\AnnotationException;
+use Doctrine\Common\Annotations\AnnotationReader;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Compiler\ServiceLocatorTagPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Exception\LogicException;
 use Symfony\Component\DependencyInjection\Exception\RuntimeException;
-use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
 
 class FeatureFlagPass implements CompilerPassInterface
@@ -19,7 +24,10 @@ class FeatureFlagPass implements CompilerPassInterface
             return;
         }
 
+        $this->autoconfigureAnnotatedFeatures($container);
+
         $features = [];
+        $serviceRefs = [];
         foreach ($container->findTaggedServiceIds('ajgarlag.feature_flag.feature') as $serviceId => $tags) {
             $className = $this->getServiceClass($container, $serviceId);
             $r = $container->getReflectionClass($className);
@@ -42,19 +50,81 @@ class FeatureFlagPass implements CompilerPassInterface
                     throw new RuntimeException(\sprintf('Invalid feature method "%s": method "%s::%s()" must be public.', $serviceId, $r->getName(), $method));
                 }
 
-                $features[$featureName] = (new Definition(\Closure::class))
-                    ->setLazy(true)
-                    ->setFactory([\Closure::class, 'fromCallable'])
-                    ->setArguments([[new Reference($serviceId), $method]]);
+                $features[$featureName] = [$serviceId, $method];
+                $serviceRefs[$serviceId] = new Reference($serviceId);
             }
         }
 
+        $locator = ServiceLocatorTagPass::register($container, $serviceRefs);
         $container->getDefinition('ajgarlag.feature_flag.provider.in_memory')
-            ->setArgument('$features', $features)
-        ;
+            ->setArguments([$locator, $features]);
 
         if ($container->hasDefinition('profiler')) {
             $this->loadDebugDefinitions($container);
+        }
+    }
+
+    private function autoconfigureAnnotatedFeatures(ContainerBuilder $container): void
+    {
+        $reader = new AnnotationReader();
+
+        foreach ($container->getDefinitions() as $definition) {
+            if (!$definition->isAutoconfigured() || $definition->isAbstract()) {
+                continue;
+            }
+
+            $class = $definition->getClass();
+            if (null === $class) {
+                continue;
+            }
+
+            try {
+                $r = $container->getReflectionClass($class, false);
+            } catch (\ReflectionException $e) {
+                continue;
+            }
+
+            if (null === $r) {
+                continue;
+            }
+
+            try {
+                $this->addAnnotationTags($reader, $r, $definition);
+            } catch (AnnotationException $e) {
+                // Ignore classes whose docblocks Doctrine cannot parse.
+                continue;
+            }
+        }
+    }
+
+    private function addAnnotationTags(AnnotationReader $reader, \ReflectionClass $r, Definition $definition): void
+    {
+        foreach ($reader->getClassAnnotations($r) as $annotation) {
+            if (!$annotation instanceof AsFeature) {
+                continue;
+            }
+
+            $definition->addTag('ajgarlag.feature_flag.feature', [
+                'feature' => $annotation->name ?? $r->getName(),
+                'method' => $annotation->method ?? '__invoke',
+            ]);
+        }
+
+        foreach ($r->getMethods() as $method) {
+            foreach ($reader->getMethodAnnotations($method) as $annotation) {
+                if (!$annotation instanceof AsFeature) {
+                    continue;
+                }
+
+                if (null !== $annotation->method && $method->getName() !== $annotation->method) {
+                    throw new LogicException(\sprintf('Using the @AsFeature(method: "%s") annotation on a method is not valid. Either remove the method value or move this to the top of the class (%s).', $annotation->method, $r->getName()));
+                }
+
+                $definition->addTag('ajgarlag.feature_flag.feature', [
+                    'feature' => $annotation->name ?? $r->getName().'::'.$method->getName(),
+                    'method' => $method->getName(),
+                ]);
+            }
         }
     }
 
@@ -75,8 +145,8 @@ class FeatureFlagPass implements CompilerPassInterface
 
     private function loadDebugDefinitions(ContainerBuilder $container): void
     {
-        $loader = new PhpFileLoader($container, new FileLocator(\dirname(__DIR__).'/../../config'));
+        $loader = new XmlFileLoader($container, new FileLocator(\dirname(__DIR__).'/../../config'));
 
-        $loader->load('feature_flag_debug.php');
+        $loader->load('feature_flag_debug.xml');
     }
 }
